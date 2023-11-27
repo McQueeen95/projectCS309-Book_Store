2de4bbb51fd0a184778bf5a9f71bb6cce31206d6
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2023-11-27 14:09:47 +0200

    Creating app.js
    arrive "Hello World" at localhost:8000

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,2 +1,8 @@
-require("./server");
 
+const { express, mongoose, app } = require("./server");
+
+// Middlewares
+
+app.use('/',(req,res,next)=>{
+  res.send("Hello World")
+})
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-
 const uri = "mongodb+srv://Ahmed_Mohamed:CARS95@cluster0.2szpgm7.mongodb.net/?retryWrites=true&w=majority"
 
 async function connectToMongoDB() {
   try {
     await mongoose.connect(uri);
     console.log("Connected to MongoDB");
+    app.listen(8000, () => {
+      console.log("Server is running on port 8000");
+    });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
@@ -15,6 +17,4 @@ async function connectToMongoDB() {
 
 connectToMongoDB();
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-});
+module.exports= {express,mongoose,app}
