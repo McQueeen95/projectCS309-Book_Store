629352b15c61069a6851961bf3c09467c4692210
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2023-12-03 21:08:24 +0200

    Adding CORS

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,8 +1,9 @@
 const { express, mongoose, app } = require("./server");
+const cors = require("cors");
 const Book = require("./models/book");
 
 
-
+app.use(cors());
 app.use(express.json()); // this allow us to use json to send and receive data
 app.use(express.urlencoded({ extended: true })); // this allow us to use url to send and receive data
 
