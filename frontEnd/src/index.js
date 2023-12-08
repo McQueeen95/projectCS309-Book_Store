ef987acd7cd9b034dbb4075aee5d9c2f568b4fd9
elshimy mahmoud <hkop9530@gmail.com>
2023-12-08 15:38:09 +0200

    new

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -1,40 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-
-import AddBook from './component/admin/AddBook';
-import AdminPage from './component/admin/AdminPage';
-import UpdateBook from './component/admin/UpdateBook';
-import DeleteBook from './component/admin/DeleteBook';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/admin", 
-    element: <AdminPage/>
-  },
-  {
-    path: "/admin/add",
-    element: <AddBook/>
-  },
-  {
-    path: "/admin/update",
-    element: <UpdateBook/>
-  },
-  {
-    path: "/admin/delete",
-    element: <DeleteBook/>
-  }
-  
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+      <App />
+   
+  </React.StrictMode>
 );
