a973b13fb12ab8e1bc53089ed79f76f1c2f48962
Ahmed-fouad2210 <150384292+Ahmed-fouad2210@users.noreply.github.com>
2023-12-08 20:03:14 +0200

    Regiater

--- frontEnd/src/Pages/register/Register.jsx
+++ frontEnd/src/Pages/register/Register.jsx
@@ -1,6 +1,51 @@
+import { Link } from "react-router-dom";
+import "./register.css";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Register=()=>{
-    return(
-        <h1>fggggggggggggggggggg</h1>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+
+        if(email.trim() === "") return toast.error("Email is required");
+        if(username.trim() === "") return toast.error("Username is required");
+        if(password.trim() === "") return toast.error("Password is required");
+
+        console.log( {email , password , username} );
+    };
+
+
+    return (
+        <div className="form-wrapper">
+            <ToastContainer/>
+            <h1 className="form-title">Create a new account</h1>
+            <form onSubmit={formSubmitHandler} className="form">
+                <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="Email" />
+                <input value={username} onChange={e => setUsername(e.target.value)} type="username" placeholder="Username" />
+                <input 
+                value={password} 
+                onChange={e => setPassword(e.target.value)} 
+                type="password" 
+                placeholder="Password" />
+                <input 
+                value={password} 
+                onChange={e => setPassword(e.target.value)} 
+                type="password" 
+                placeholder="Confirm Password" />
+
+                
+                <button className="form-btn">Register</button>
+            </form>
+            <div className="form-footer">
+                Already have an account ?{" "}
+                <Link to="/login" className="form-link" >Login</Link>
+            </div>
+        </div>
     )
 }
 export default Register;
