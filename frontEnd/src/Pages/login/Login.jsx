56ea5d40a551358463de408c34d3cf0039b615d6
Ahmed-fouad2210 <150384292+Ahmed-fouad2210@users.noreply.github.com>
2023-12-08 18:02:32 +0200

    Rest of Login and its Validation

--- frontEnd/src/Pages/login/Login.jsx
+++ frontEnd/src/Pages/login/Login.jsx
@@ -1,6 +1,37 @@
-const Login=()=>{
-    return(
-        <h1>login</h1>
+import { Link } from "react-router-dom";
+import "./login.css";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+
+        if(email.trim() === "") return toast.error("Email is required");
+        if(password.trim() === "") return toast.error("Password is required");
+
+        console.log( {email , password } );
+    };
+
+    return (
+        <div className="form-wrapper">
+            <ToastContainer/>
+            <h1 className="form-title">Login to your account</h1>
+            <form onSubmit={formSubmitHandler} className="form">
+                <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="Email" />
+                <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+                <button className="form-btn">Login</button>
+            </form>
+            <div className="form-footer">
+                Don't have an account ?{" "}
+                <Link to="/register" className="form-link" >Register</Link>
+            </div>
+        </div>
     )
 }
 export default Login;
